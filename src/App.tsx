c9f785742df370a66c32c23335a715f5a0f52094
raiane-honorato <raianehonorato16@gmail.com>
2022-06-22 19:31:44 -0300

    feat(ignite): create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
-import { gql, useQuery } from "@apollo/client";
+import { Event } from "./pages/Event";
 
 function App() {
 
-  const GET_LESSONS_QUERY = gql`
-    query {
-      lessons {
-        id
-        title
-
-        teacher {
-          name
-          bio
-        }
-      }
-    }
-  `
-
-  interface Lesson {
-    id: string;
-    title: string;
-  }
-
-  const { data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY);
-
   return (
-    <ul>
-      {data?.lessons.map(lesson => <li key={lesson.id}>{lesson.title}</li>)}
-    </ul>
+    <>
+      <Event />
+    </>
   )
 }
 
